4c42b0007833a1d613537eb9c916dd8f95f77d9c
zaki khan <mohammedkhanzaki@gmail.com>
2024-05-22 20:28:26 +0530

    ELASTIC BEAN STALK ENDPOINT

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,5 @@
 export const environment = {
-    baseApiUrl: 'https://localhost:7279/'
+    //baseApiUrl: 'https://localhost:7279/'
+    baseApiUrl: 'http://development.eba-4jfeshjk.ap-south-1.elasticbeanstalk.com/'
+    
 };
